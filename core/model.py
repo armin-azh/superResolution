fb4aa8f4b8e86302e243002c2cab8d70404eaec6
amirhosein ahmadnejad <a.ah.r1378@hotmail.com>
2021-10-08 11:38:51 +0330

    edit2

--- core/model.py
+++ core/model.py
@@ -11,14 +11,14 @@ class SuperResolutionModel(nn.Module):
 
         # first block
         self._up_sample = nn.Upsample(scale_factor=(2, 2))
-        self._conv1 = nn.ConvTranspose2d(in_channels=3, out_channels=64, kernel_size=(15, 15))
-        self._conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(11, 11))
-        self._conv3 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=(11, 11))
+        self._conv1 = nn.ConvTranspose2d(in_channels=3, out_channels=64, kernel_size=(21, 21))
+        self._conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(17, 17))
+        self._conv3 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=(17, 17))
 
         # second bock
-        self._conv4 = nn.ConvTranspose2d(in_channels=9, out_channels=64, kernel_size=(9, 9))
-        self._conv5 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(5, 5))
-        self._conv6 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=(5, 5))
+        self._conv4 = nn.ConvTranspose2d(in_channels=9, out_channels=64, kernel_size=(21, 21))
+        self._conv5 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(17, 17))
+        self._conv6 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=(17, 17))
 
         self._relu = nn.ReLU(inplace=True)
 
